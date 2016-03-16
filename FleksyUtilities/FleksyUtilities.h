4c99a4197a6b15c094cd1a463940e29196c10cdc
Ben Pious <benpious@gmail.com>
2016-03-16 13:42:03 -0700

    Initial Commit

---
+++ FleksyUtilities/FleksyUtilities.h
@@ -0,0 +1,19 @@
+//
+//  FleksyUtilities.h
+//  FleksyUtilities
+//
+//  Created by Ben Pious on 3/16/16.
+//  Copyright © 2016 Fleksy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FleksyUtilities.
+FOUNDATION_EXPORT double FleksyUtilitiesVersionNumber;
+
+//! Project version string for FleksyUtilities.
+FOUNDATION_EXPORT const unsigned char FleksyUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FleksyUtilities/PublicHeader.h>
+
+
